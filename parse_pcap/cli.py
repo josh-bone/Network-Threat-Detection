32fddcb2835d11773b5e70032d7aac4a3699b6c6
josh-bone <bone.j@northeastern.edu>
2025-10-02 20:11:24 -0400

    typo

--- parse_pcap/cli.py
+++ parse_pcap/cli.py
@@ -115,7 +115,7 @@ def main():
     ):
         _ = capture_packets(
             output_filename=args.pcap_file,
-            bpl_filter=args.filter,
+            bpf_filter=args.filter,
             interface=args.capture_interface,
             duration=args.capture_duration,
         )
